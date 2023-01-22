055d43394fc55602bff24adbf1a8d99223b3b3e0
Liam Lenholm <lenholmliam@gmail.com>
2023-01-22 22:03:10 +0100

    Frontend updates: Delete Client Mutation and Apollo Cache

--- 1-mern-graphql-project-mgmt-system/client/src/App.js
+++ 1-mern-graphql-project-mgmt-system/client/src/App.js
@@ -1,12 +1,40 @@
 import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Clients from "./components/Clients";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(exsisting, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(exsisting, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
