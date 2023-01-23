f035dfe39d99546464695bfe1db8c940522f6979
Liam Lenholm <lenholmliam@gmail.com>
2023-01-23 19:40:06 +0100

    Add Project n Delete Project

--- 1-mern-graphql-project-mgmt-system/client/src/pages/Project.jsx
+++ 1-mern-graphql-project-mgmt-system/client/src/pages/Project.jsx
@@ -3,6 +3,7 @@ import { GET_PROJECT } from "../queries/projectQueries";
 import Spinner from "../components/Spinner";
 import { useQuery } from "@apollo/client";
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
 
 export default function Project() {
   const { id } = useParams();
@@ -28,6 +29,8 @@ export default function Project() {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
